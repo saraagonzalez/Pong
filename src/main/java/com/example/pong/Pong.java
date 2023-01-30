d2b8da1b578e098952c16163b57df490d7c3f2b6
saraagonzalez <saraxingonzalezalfageme_7240@escuelaces.net>
2023-01-30 10:14:39 +0100

    un poco de funcionalidad

---
+++ src/main/java/com/example/pong/Pong.java
@@ -0,0 +1,15 @@
+package com.example.pong;
+
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+public class Pong {
+    public static void handLePlayer(Rectangle player1, double playerVelY) {
+    }
+
+    public static void updateGame(Rectangle player1, Rectangle player2, Circle ball) {
+    }
+
+    public static void resetGame() {
+    }
+}
