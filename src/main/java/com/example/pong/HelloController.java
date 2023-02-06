6284a0becf41571c48e391ed4e4029f81c54cd40
saraagonzalez <saraxingonzalezalfageme_7240@escuelaces.net>
2023-02-06 09:03:22 +0100

    juego

--- src/main/java/com/example/pong/HelloController.java
+++ src/main/java/com/example/pong/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.pong;
 
 import javafx.animation.AnimationTimer;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -11,105 +12,6 @@ import javafx.scene.shape.Rectangle;
 
 public class HelloController {
 
-    @FXML
-    private Pane board;
 
-    @FXML
-    private Rectangle player1;
-
-    @FXML
-    private Rectangle player2;
-
-    @FXML
-    private Circle ball;
-
-    @FXML
-    private Label score1;
-
-    @FXML
-    private Label score2;
-
-    private AnimationTimer timer;
-    private double playerVelY = 0;
-    private double player2VelY = 0;
-
-    public HelloController() {
-        timer = new AnimationTimer() {
-            @Override
-            public void handle(long arg0) {
-                Pong.handLePlayer(player1, playerVelY);
-                Pong.handLePlayer(player2, player2VelY);
-                Pong.updateGame(player1, player2, ball);
-                checkEndGame();
-            }
-        };
-    }
-
-    @FXML
-    public void initialize() {
-
-    }
-
-    @FXML
-    void play() {
-        board.requestFocus();
-        timer.start();
-    }
-
-    @FXML
-    void reset(ActionEvent event) {
-        timer.stop();
-        ball.setCenterX(400);
-        ball.setCenterY(230);
-        player1.setX(10);
-        player1.setY(170);
-        player2.setX(780);
-        player1.setY(170);
-        Pong.resetGame();
-    }
-
-    @FXML
-    void onKeyPressed(KeyEvent event) {
-        switch (event.getCode()) {
-            case W:
-                if (player1.getY() >= 10) {
-                    playerVelY = -10;
-                    player1.setY(player1.getY() + playerVelY);
-                }
-                break;
-            case S:
-                if (player1.getY() <= 600) {
-                    playerVelY = 10;
-                    player1.setY(player1.getY() + playerVelY);
-                }
-                break;
-            case J:
-                if (player2.getY() >= 600) {
-                    player2VelY = 10;
-                    player2.setY(player2.getY() + player2VelY);
-                }
-                break;
-            case N:
-                if (player2.getY() <= 600) {
-                    player2VelY = 10;
-                    player2.setY(player2.getY() + player2VelY);
-                }
-                break;
-            default:
-                break;
-
-        }
-    }
-
-    @FXML
-    void onKeyReleased(KeyEvent event) {
-
-    }
-
-    public void checkEndGame() {
-        if (ball.getCenterX() < 15) {
-            timer.stop();
-        }
-    }
 
 }
--- src/main/java/com/example/pong/Main.java
+++ src/main/java/com/example/pong/Main.java
@@ -1,25 +1,163 @@
-//https://www.youtube.com/watch?v=h9NL7wAyw4E&ab_channel=Odyss%C3%A9eD%27unBit
-
 package com.example.pong;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import java.io.IOException;
+import javafx.util.Duration;
 
 public class Main extends Application {
+    private static final int WIDTH = 1000;
+    private static final int HEIGHT = 600;
+
+    private double ballX = 500;
+    private double ballY = 300;
+    private double ballSpeedX = 3;
+    private double ballSpeedY = 3;
+    private Circle ball;
+
+    private double paddle1Y = HEIGHT / 2;
+    private Rectangle paddle1;
+
+    private double paddle2Y = HEIGHT / 2;
+    private Rectangle paddle2;
+
+    private boolean upPressed;
+    private boolean downPressed;
+    private boolean wPressed;
+    private boolean sPressed;
+
+    int score1 = 0;
+    int score2 = 0;
+
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("sample.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("PONG");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        ball = new Circle(ballX, ballY, 10, Color.WHITE);
+
+        paddle1 = new Rectangle(10, paddle1Y, 10, 120);
+        paddle1.setFill(Color.WHITE);
+
+        paddle2 = new Rectangle(WIDTH - 20, paddle2Y, 10, 120);
+        paddle2.setFill(Color.WHITE);
+
+        Text score = new Text();
+        score.setStyle("-fx-text-inner-color: red;");
+        score.setFont(Font.font(30));
+        score.setX(300);
+        score.setY(90);
+
+        Pane root = new Pane();
+        root.getChildren().addAll(ball, paddle1, paddle2);
+
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+        scene.setFill(Color.GREEN);
+
+        scene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.W) {
+                upPressed = true;
+            } else if (event.getCode() == KeyCode.S) {
+                downPressed = true;
+            } else if (event.getCode() == KeyCode.UP) {
+                wPressed = true;
+            } else if (event.getCode() == KeyCode.DOWN) {
+                sPressed = true;
+            }
+        });
+
+        scene.setOnKeyReleased(event -> {
+            if (event.getCode() == KeyCode.W) {
+                upPressed = false;
+            } else if (event.getCode() == KeyCode.S) {
+                downPressed = false;
+            } else if (event.getCode() == KeyCode.UP) {
+                wPressed = false;
+            } else if (event.getCode() == KeyCode.DOWN) {
+                sPressed = false;
+            }
+        });
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(10), e -> {
+            if (upPressed) {
+                paddle1Y -= 8;
+            }
+            if (downPressed) {
+                paddle1Y += 8;
+            }
+            if (wPressed) {
+                paddle2Y -= 8;
+            }
+            if (sPressed) {
+                paddle2Y += 8;
+            }
+            ballX += ballSpeedX;
+            ballY += ballSpeedY;
+
+            if (ballX <= 20) {
+                if (ballY > paddle1Y && ballY < paddle1Y + 50) {
+                    ballSpeedX = -ballSpeedX;
+                } else {
+                    score1++;
+                    score.setText("Player 1: " + score2 + "  Player 2: " + score1);
+                    ballX = 500;
+                    ballY = 300;
+                    ballSpeedX = 3;
+                    ballSpeedY = 3;
+                }
+            }
+            if (ballX >= 975) {
+                if (ballY > paddle2Y && ballY < paddle2Y + 50) {
+                    ballSpeedX = -ballSpeedX;
+                } else {
+                    score2++;
+                    score.setText("Player 1: " + score2 + "  Player 2: " + score1);
+                    ballX = 500;
+                    ballY = 300;
+                    ballSpeedX = 3;
+                    ballSpeedY = 3;
+                }
+            }
+            if (ballY <= 10 || ballY >= 400) {
+                ballSpeedY = -ballSpeedY;
+            }
+            if (paddle1Y <= 0) {
+                paddle1Y = 0;
+            } else if (paddle1Y >= 390) {
+                paddle1Y = 390;
+            }
+
+            if (paddle2Y <= 0) {
+                paddle2Y = 0;
+            } else if (paddle2Y >= 600) {
+                paddle2Y = 600;
+            }
+            paddle1.setY(paddle1Y);
+            paddle2.setY(paddle2Y);
+            ball.setCenterX(ballX);
+            ball.setCenterY(ballY);
+        }));
+
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+        score.setText("Player 1: " + score2 + "  Player 2: " + score1);
+        root.getChildren().add(score);
+
+
+        primaryStage.setTitle("PONG");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
+
